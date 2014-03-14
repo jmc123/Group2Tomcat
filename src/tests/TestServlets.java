5e6a4b8c88b20a9407914de224d86e894d13b09c
zodac <arouge110@msn.com>
2014-03-14 11:34:31 +0000

    Added Mockito JUnit test for RegisterServlet. Added mockito.jar.

---
+++ src/tests/TestServlets.java
@@ -0,0 +1,58 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import main.RegisterServlet;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import persistence.PersistenceUtil;
+
+public class TestServlets {
+	
+	@BeforeClass
+	public static void setUp(){
+		PersistenceUtil.useTestDatabase();
+	}
+
+	@Test
+	public void testRegisterServlet() throws ServletException, IOException {
+		HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        
+        when(request.getParameter("userName")).thenReturn("userMock");
+        when(request.getParameter("password")).thenReturn("pass");
+        when(request.getParameter("role")).thenReturn("1");
+        when(request.getParameter("fname")).thenReturn("fname");
+        when(request.getParameter("lname")).thenReturn("lname");
+        when(request.getParameter("email")).thenReturn("mock@mock.com");
+        when(request.getParameter("phone")).thenReturn("01111111");
+		
+		when(response.getWriter()).thenReturn(new PrintWriter(new StringWriter()));
+
+        new RegisterServlet().doPost(request, response);
+
+        assertEquals("Should be 'userMock'", "userMock", PersistenceUtil.findUserByUsername("userMock").getUserName());
+	}
+	
+	@Test
+	public void testLoginServletLogin(){
+		
+	}
+	
+	@Test
+	public void testLoginServletLogout(){
+		
+	}
+}
